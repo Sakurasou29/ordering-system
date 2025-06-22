e9133730f03bda75d3ccfeb62c5e69e7752821ce
Sakurasou29 <cherise.quirolo@gmail.com>
2025-06-22 20:41:15 +0200

    Add manifest and service worker for PWA support

--- service-worker.js
+++ service-worker.js
@@ -1,32 +1,37 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2822
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fmodern\fcharset0 Courier;}
-{\colortbl;\red255\green255\blue255;\red0\green0\blue0;}
-{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\partightenfactor0
+const CACHE_NAME = 'kiosk-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/styles.css',   // if you have a separate CSS file
+  '/script.js',    // if you have a separate JS file
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
 
-\f0\fs26 \cf0 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 self.addEventListener('install', event => \{\
-  event.waitUntil(\
-    caches.open('ordering-system-v1').then(cache => \{\
-      return cache.addAll([\
-        '/',\
-        '/index.html',\
-        '/manifest.json',\
-        '/icon-192.png',\
-        '/icon-512.png'\
-        // add other static files here\
-      ]);\
-    \})\
-  );\
-\});\
-\
-self.addEventListener('fetch', event => \{\
-  event.respondWith(\
-    caches.match(event.request).then(response => \{\
-      return response || fetch(event.request);\
-    \})\
-  );\
-\});\
-}
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
